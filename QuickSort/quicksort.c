63c38389b598fd5e237caca1e54cb83384e1b19e
InversionSpaces <InversionSpaces@vivaldi.net>
2019-09-09 13:12:35 +0300

    Spaces in quicksort

--- QuickSort/quicksort.c
+++ QuickSort/quicksort.c
@@ -7,18 +7,18 @@ void swap(int* a, int* b) {
 }
 
 void quicksort(int* array, size_t size) {
-	if(size < 2) return;
+	if (size < 2) return;
 	
 	size_t l = 0, r = size - 1, p = (l + r) / 2;
-	while(l < r) {	
-		while(array[l] < array[p] && l < p) l++;
-		while(array[r] >= array[p] && p < r) r--;
-		if(l == r) break;
+	while (l < r) {	
+		while (array[l] < array[p] && l < p) l++;
+		while (array[r] >= array[p] && p < r) r--;
+		if (l == r) break;
 		
 		swap(&array[l], &array[r]);
 		
-		if(l == p) p = r;
-		else if(r == p) p = l;
+		if (l == p) p = r;
+		else if (r == p) p = l;
 	}
 	
 	quicksort(array, p);
