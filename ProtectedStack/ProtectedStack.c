5b97570013a5df2c0801a2b4a2fdd1960c24b994
InversionSpaces <InversionSpaces@vivaldi.net>
2019-09-27 20:21:22 +0300

    Added hash support

--- ProtectedStack/ProtectedStack.c
+++ ProtectedStack/ProtectedStack.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <inttypes.h>
+#include <string.h>
 
 #ifndef PS_NDEBUG
 #define DEAD_BYTE (0x4D)
@@ -49,7 +50,8 @@ typedef enum {
 	BEFORE_PUSH		= 1 << 4,
 	AFTER_PUSH		= 1 << 5,
 	BEFORE_POP		= 1 << 6,
-	AFTER_POP		= 1 << 7
+	AFTER_POP		= 1 << 7,
+	BEFORE_HASH		= 1 << 8
 } PS_CHECK_REASON;
 
 #define ASSERT_EXIT_CODE (2)
@@ -71,6 +73,7 @@ typedef enum {
 #define PStackInitMACRO(stackp, capacity) {			\
 	PStackInit(stackp, capacity);					\
 	(stackp)->name = #stackp;						\
+	(stackp)->hash = PStackCalcHash(stackp);		\
 }
 #else
 #define PStackInitMACRO(stackp, capacity) {			\
@@ -80,10 +83,56 @@ typedef enum {
 
 #define PStackDumpMACRO(stackp) 					\
 		PStackDump(stackp, PStackCheck(stackp, COMMON));
+		
+void HashLyAdd(uint32_t* hash, const void* data, size_t size)
+{
+    for(const uint8_t* i = (const uint8_t*)data; --size; ++i)
+        *hash = (*hash * 1664525) + (unsigned char)(*i) + 1013904223;
+}
+
+// TODO DOCS
+uint32_t PStackCalcHash(PStack_t* stackp);
+
+// TODO DOCS
+PS_ERROR PStackCheck(PStack_t* stackp, PS_CHECK_REASON reason);
+
+// TODO DOCS
+void PStackDump(PStack_t* stackp, PS_ERROR error);
+
+uint32_t PStackCalcHash(PStack_t* stackp)
+{
+	PS_ASSERT(stackp, BEFORE_HASH)
+	
+	uint32_t retval = 0;
+
+#ifndef PS_NDEBUG	
+	HashLyAdd(&retval, &stackp->front_guard, 
+		sizeof(stackp->front_guard));
+		
+	HashLyAdd(&retval, &stackp->back_guard, 
+		sizeof(stackp->back_guard));
+		
+	HashLyAdd(&retval, stackp->name, 
+		strlen(stackp->name));
+#endif
+
+	HashLyAdd(&retval, &stackp->capacity, 
+		sizeof(stackp->capacity));
+		
+	HashLyAdd(&retval, &stackp->size, 
+		sizeof(stackp->size));
+		
+	HashLyAdd(&retval, stackp->array, 
+		sizeof(stack_el_t) * stackp->capacity);
+		
+	return retval;
+}
 
 PS_ERROR PStackCheck(PStack_t* stackp, PS_CHECK_REASON reason)
 {
 	PS_ERROR retval = NO_ERROR;
+	
+	//printf("In check error %u\n", retval);
 	
 	if (stackp == NULL) {
 		retval |= NULL_STACKP;
@@ -93,14 +142,21 @@ PS_ERROR PStackCheck(PStack_t* stackp, PS_CHECK_REASON reason)
 	if (reason & ~BEFORE_INIT) {
 		if (stackp->capacity < stackp->size)
 			retval |= TOO_BIG_SIZE;
+				
 		if (stackp->capacity == 0)
 			retval |= NULL_CAPACITY;
+			
 		if (stackp->array == NULL)
 			retval |= NULL_ARRAYP;
+			
 #ifndef PS_NDEBUG
 		if (stackp->front_guard != GUARD_BYTE ||
 			stackp->back_guard != GUARD_BYTE)
 			retval |= GUARD_GORRUPTED;
+			
+		if (reason & ~BEFORE_HASH)
+			if (PStackCalcHash(stackp) != stackp->hash)
+				retval |= HASH_NOT_MATCH;
 #endif
 	}
 	
@@ -142,6 +198,8 @@ int IsDead(const void* ptr, size_t size)
 		printf("## TOO SMALL SIZE;\n");			\
 	if (error & GUARD_GORRUPTED)				\
 		printf("## GUARD BYTE CORRUPTED;\n");	\
+	if (error & HASH_NOT_MATCH)					\
+		printf("## HASH DOESNT MATCH;\n");	\
 }
 
 #define PRINT_CAPACITY_AND_SIZE(stackp)			\
@@ -155,18 +213,11 @@ int IsDead(const void* ptr, size_t size)
 #define PRINT_NAME(stackp) printf("## No name support;\n");
 #endif
 
-unsigned int HashLyAdd(unsigned int hash, const void* data, size_t size)
-{
-    for(const uint8_t* i = (const uint8_t*)data; ; str++)
-        hash = (hash * 1664525) + (unsigned char)(*str) + 1013904223;
-
-    return hash;
-}
-
 void PStackDump(PStack_t* stackp, PS_ERROR error)
 {
 	printf("## STACK DUMP;\n");
 	
+	//printf("%lu\n", error);
 	PRINT_ERRORS(error);
 	
 	if (error & NULL_STACKP)
@@ -207,16 +258,24 @@ void PStackInit(PStack_t* stackp, size_t capacity)
 	
 	stackp->front_guard = GUARD_BYTE;
 	stackp->back_guard	= GUARD_BYTE;
+	
+	stackp->name = "[Default stack name]";
+	
+	stackp->hash = PStackCalcHash(stackp);
 #endif
 	
 	PS_ASSERT(stackp, AFTER_INIT)
 }
 
 void PStackPush(PStack_t* stackp, stack_el_t elem)
-{	
+{
 	PS_ASSERT(stackp, BEFORE_PUSH);
 	
 	stackp->array[stackp->size++] = elem;
+	
+#ifndef PS_NDEBUG
+	stackp->hash = PStackCalcHash(stackp);
+#endif
 
 	PS_ASSERT(stackp, AFTER_PUSH);
 }
@@ -229,6 +288,8 @@ stack_el_t PStackPop(PStack_t* stackp)
 	
 #ifndef PS_NDEBUG
 	memset(stackp->array + stackp->size, DEAD_BYTE, sizeof(stack_el_t));
+
+	stackp->hash = PStackCalcHash(stackp);
 #endif
 	
 	PS_ASSERT(stackp, AFTER_POP);
@@ -255,7 +316,7 @@ void PStackDeInit(PStack_t* stackp)
 }
 
 int main()
-{
+{	
 	PStack_t s = {};
 	PStackInitMACRO(&s, 15);
 	
@@ -265,7 +326,9 @@ int main()
 	PStackPop(&s);	
 	PStackPush(&s, 0x4D4D4D4D);
 	
-	s.front_guard = 0;
+	s.array[10] = 100;
+	
+	//s.back_guard = 0;
 	
 	//PStackDumpMACRO(&s);
 		
