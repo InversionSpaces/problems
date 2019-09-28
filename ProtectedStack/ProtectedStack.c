faf9352ea6d859be0f01a50a1f60adc115a60791
InversionSpaces <InversionSpaces@vivaldi.net>
2019-09-28 14:24:38 +0300

    Docs and better dump

--- ProtectedStack/ProtectedStack.c
+++ ProtectedStack/ProtectedStack.c
@@ -83,21 +83,180 @@ typedef enum {
 
 #define PStackDumpMACRO(stackp) 					\
 		PStackDump(stackp, PStackCheck(stackp, COMMON));
-		
+
+/*! Функция аддитивного хэширования
+ * @param [in, out] hash Указатель на переменную хэша. Начальное значение - 0
+ * @param [in] data Указатель на хэшируемые данные
+ * @param [in] size Размер данных в байтах
+ */
 void HashLyAdd(uint32_t* hash, const void* data, size_t size)
 {
+	assert(hash != NULL);
+	assert(data != NULL);
+	
     for(const uint8_t* i = (const uint8_t*)data; --size; ++i)
         *hash = (*hash * 1664525) + (unsigned char)(*i) + 1013904223;
 }
 
-// TODO DOCS
+/*! Функция проверки данных на то, являются ли они мёртвыми
+ * @param [in] ptr Указатель на данные
+ * @param [in] size Размер данных
+ * @return 1, если они мёртвые, 0 - иначе
+ * @note Если существует PS_NDEBUG, все данные считаются не мёртвыми
+ */
+int IsDead(const void* ptr, size_t size);
+
+/*! Функция инициализации стэка
+ * @param [in, out] stackp Указатель на стэк
+ * @param [in] capacity Максимальный размер стэка в элементах
+ */
+void PStackInit(PStack_t* stackp, size_t capacity);
+
+/*! Функция добавления в стэк
+ * @param [in] stackp Указатель на стэк
+ * @param [in] elem Добавляемое значение
+ */
+void PStackPush(PStack_t* stackp, stack_el_t elem);
+
+/*! Функция удаления из стэка
+ * @param [in] stackp Указатель на стэк
+ * @return Удалённое значение
+ */
+stack_el_t PStackPop(PStack_t* stackp);
+
+/*! Функция проверки стэка на пустоту
+ * @param [in] stackp Указатель на стэк
+ * @return 1 - если стэк пуст, 0 - иначе
+ */
+int PStackIsEmpty(PStack_t* stackp);
+
+/*! Функция деинициализации стэка
+ * @param [in] stackp Указатель на стэк
+ */
+void PStackDeInit(PStack_t* stackp);
+
+/*! Функция вычисления хэша стэка
+ * @param [in] stackp Указатель на стэк
+ * @return Хэш стэка
+ */
 uint32_t PStackCalcHash(PStack_t* stackp);
 
-// TODO DOCS
+/*! Функция проверки стэка на валидность
+ * @param [in] stackp Указатель на стэк
+ * @param [in] reason Причина проверки
+ * @return Ошибка валидности стэка
+ * @note Чтобы просто проверить стэк можно использовать причину COMMON
+ */
 PS_ERROR PStackCheck(PStack_t* stackp, PS_CHECK_REASON reason);
 
-// TODO DOCS
+/*! Функция дампа стэка в stdout
+ * @param [in] stackp Указатель на стэк
+ */
 void PStackDump(PStack_t* stackp, PS_ERROR error);
+
+#define PRINT_ERRORS(error) {					\
+	if (error == NO_ERROR)						\
+		printf("## NO ERROR;\n");				\
+	else 										\
+		printf("## ERRORS OCCURED:\n");			\
+	if (error & NULL_STACKP)					\
+		printf("## NULL STACK POINTER;\n");		\
+	if (error & NULL_ARRAYP)					\
+		printf("## NULL ARRAY POINTER;\n");		\
+	if (error & NULL_CAPACITY)					\
+		printf("## NULL CAPACITY;\n");			\
+	if (error & TOO_BIG_SIZE)					\
+		printf("## TOO BIG SIZE;\n");			\
+	if (error & TOO_SMALL_SIZE)					\
+		printf("## TOO SMALL SIZE;\n");			\
+	if (error & GUARD_GORRUPTED)				\
+		printf("## GUARD BYTE CORRUPTED;\n");	\
+	if (error & HASH_NOT_MATCH)					\
+		printf("## HASH DOESNT MATCH;\n");		\
+}
+
+#define PRINT_CAPACITY_AND_SIZE(stackp)				\
+	printf("## Capacity: %lu;\n## Size: %lu;\n",	\
+				stackp->capacity, stackp->size);
+
+#ifndef PS_NDEBUG			
+#define PRINT_GUARDS(stackp)						\
+	printf("## Front guard: %u (Should be %u);\n", 	\
+		(stackp)->front_guard, GUARD_BYTE);			\
+	printf("## Back guard: %u (Should be %u);\n", 	\
+		(stackp)->back_guard, GUARD_BYTE);
+#else
+#define PRINT_GUARDS(stackp)						\
+	printf("## No guards support\n");
+#endif
+
+#ifndef PS_NDEBUG
+#define PRINT_HASH(stackp)						\
+	printf("## Hash: %u (Should be %u);\n", 	\
+		stackp->hash, PStackCalcHash(stackp));
+#else 													
+#define PRINT_HASH(stackp) 						\
+	printf("## No hash support;\n");
+#endif
+
+#define PRINT_ELEMENTS(stackp) {								\
+	printf("## Elements:\n");									\
+	for (size_t i = 0; i < stackp->size; i++) {					\
+		printf("## + [%lu] %d", i, stackp->array[i]);			\
+		if (IsDead(stackp->array + i, sizeof(stack_el_t)))		\
+			printf(" (POSSIBLY DEAD)");							\
+		printf("\n");											\
+	}															\
+	for (size_t i = stackp->size; i < stackp->capacity; i++) {	\
+		printf("## - [%lu] %d", i, stackp->array[i]);			\
+		if (IsDead(stackp->array + i, sizeof(stack_el_t)))		\
+			printf(" (DEAD)");									\
+		printf("\n");											\
+	}															\
+}
+				
+#ifndef PS_NDEBUG
+#define PRINT_NAME(stackp) printf("## Stack name: %s;\n", \
+										stackp->name);
+#else
+#define PRINT_NAME(stackp) printf("## No name support;\n");
+#endif
+
+int main()
+{	
+	PStack_t s = {};
+	PStackInitMACRO(&s, 15);
+	
+	for (int i = 0; i < 4; ++i)
+		PStackPush(&s, i);
+		
+	PStackPop(&s);	
+	PStackPush(&s, 0x4D4D4D4D);
+	
+	s.array[10] = 100;
+	
+	//s.back_guard = 0;
+	
+	//PStackDumpMACRO(&s);
+		
+	for (int i = 0; i < 4; ++i)
+		printf("%d ", PStackPop(&s));
+	printf("\nDone\n");
+		
+	//PStackDumpMACRO(&s);
+}
+
+int IsDead(const void* ptr, size_t size) 
+{
+#ifndef PS_NDEBUG
+	for (uint8_t* tmp = (uint8_t*)ptr; --size; ++tmp)
+		if (*tmp != DEAD_BYTE)
+			return 0;
+	return 1;
+#else
+	return 0;
+#endif
+}
 
 uint32_t PStackCalcHash(PStack_t* stackp)
 {
@@ -171,78 +330,22 @@ PS_ERROR PStackCheck(PStack_t* stackp, PS_CHECK_REASON reason)
 	return retval;
 }
 
-int IsDead(const void* ptr, size_t size) 
-{
-#ifndef PS_NDEBUG
-	for (uint8_t* tmp = (uint8_t*)ptr; --size; ++tmp)
-		if (*tmp != DEAD_BYTE)
-			return 0;
-	return 1;
-#else
-	return 0;
-#endif
-}
-
-#define PRINT_ERRORS(error) {					\
-	if (error == NO_ERROR)						\
-		printf("## NO ERROR;\n");				\
-	if (error & NULL_STACKP)					\
-		printf("## NULL STACK POINTER;\n");		\
-	if (error & NULL_ARRAYP)					\
-		printf("## NULL ARRAY POINTER;\n");		\
-	if (error & NULL_CAPACITY)					\
-		printf("## NULL CAPACITY;\n");			\
-	if (error & TOO_BIG_SIZE)					\
-		printf("## TOO BIG SIZE;\n");			\
-	if (error & TOO_SMALL_SIZE)					\
-		printf("## TOO SMALL SIZE;\n");			\
-	if (error & GUARD_GORRUPTED)				\
-		printf("## GUARD BYTE CORRUPTED;\n");	\
-	if (error & HASH_NOT_MATCH)					\
-		printf("## HASH DOESNT MATCH;\n");	\
-}
-
-#define PRINT_CAPACITY_AND_SIZE(stackp)			\
-	printf("## Capacity: %lu; Size: %lu;\n",	\
-				stackp->capacity, stackp->size);
-				
-#ifndef PS_NDEBUG
-#define PRINT_NAME(stackp) printf("## Stack name: %s;\n", \
-										stackp->name);
-#else
-#define PRINT_NAME(stackp) printf("## No name support;\n");
-#endif
-
 void PStackDump(PStack_t* stackp, PS_ERROR error)
 {
 	printf("## STACK DUMP;\n");
-	
-	//printf("%lu\n", error);
-	PRINT_ERRORS(error);
+
+	PRINT_ERRORS(error)
 	
 	if (error & NULL_STACKP)
 		return;
 		
-	PRINT_NAME(stackp);
-	PRINT_CAPACITY_AND_SIZE(stackp);
+	PRINT_NAME(stackp)
+	PRINT_CAPACITY_AND_SIZE(stackp)
+	PRINT_GUARDS(stackp)
+	PRINT_HASH(stackp)
 	
-	if (error == NO_ERROR) {
-		for (size_t i = 0; i < stackp->size; i++) {
-			printf("## + [%lu] %d", i, stackp->array[i]);
-			if (IsDead(stackp->array + i, sizeof(stack_el_t)))
-				printf(" (POSSIBLY DEAD)");
-			printf("\n");
-		}
-		
-		for (size_t i = stackp->size; i < stackp->capacity; i++) {
-			printf("## - [%lu] %d", i, stackp->array[i]);
-			if (IsDead(stackp->array + i, sizeof(stack_el_t)))
-				printf(" (DEAD)");
-			printf("\n");
-		}
-		
-		return;
-	}
+	if (!(error & (NULL_ARRAYP | TOO_BIG_SIZE | TOO_SMALL_SIZE)))
+		PRINT_ELEMENTS(stackp)
 }
 
 void PStackInit(PStack_t* stackp, size_t capacity)
@@ -313,28 +416,4 @@ void PStackDeInit(PStack_t* stackp)
 	stackp->size 		= 0;
 	stackp->capacity 	= 0;
 	stackp->array		= NULL;
-}
-
-int main()
-{	
-	PStack_t s = {};
-	PStackInitMACRO(&s, 15);
-	
-	for (int i = 0; i < 4; ++i)
-		PStackPush(&s, i);
-		
-	PStackPop(&s);	
-	PStackPush(&s, 0x4D4D4D4D);
-	
-	s.array[10] = 100;
-	
-	//s.back_guard = 0;
-	
-	//PStackDumpMACRO(&s);
-		
-	for (int i = 0; i < 4; ++i)
-		printf("%d ", PStackPop(&s));
-	printf("\nDone\n");
-		
-	//PStackDumpMACRO(&s);
 }
