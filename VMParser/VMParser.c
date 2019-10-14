201b5139b12dfa6267f587fca97a97e342d665d8
InversionSpaces <InversionSpaces@vivaldi.net>
2019-10-14 23:31:44 +0300

    VMParser

--- VMParser/VMParser.c
+++ VMParser/VMParser.c
@@ -5,6 +5,12 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+#include "exitingalloc.h"
+#include "commands.h"
+#include "tokenizer.h"
+#include "binaryfile.h"
+
+
 FILE *exiting_fopen(const char *fname, const char *mod)
 {
 	assert(fname != NULL);
@@ -15,39 +21,6 @@ FILE *exiting_fopen(const char *fname, const char *mod)
 	if (retval == NULL) {
 		printf("# ERROR: Failed to open file: %s. Exiting...\n", fname);
 		exit(1);
-	}
-
-	return retval;
-}
-
-void *exiting_calloc(size_t n, size_t size)
-{
-	void *retval = calloc(n, size);
-	if (retval == NULL) {
-		printf("# ERROR: Failed to calloc memory. Exiting...\n");
-		exit(2);
-	}
-
-	return retval;
-}
-
-void *exiting_malloc(size_t size)
-{
-	void *retval = malloc(size);
-	if (retval == NULL) {
-		printf("# ERROR: Failed to malloc memory. Exiting...\n");
-		exit(2);
-	}
-
-	return retval;
-}
-
-void *exiting_realloc(void *ptr, size_t size)
-{
-	void *retval = realloc(ptr, size);
-	if (retval == NULL) {
-		printf("# ERROR: Failed to realloc memory. Exiting...\n");
-		exit(2);
 	}
 
 	return retval;
@@ -99,125 +72,8 @@ char *read_file(const char *filename)
 	return retval;
 }
 
-char *next_line(char *cur_line)
-{
-	assert(cur_line != NULL);
-	
-	for (; *cur_line; ++cur_line) {
-		if (*cur_line == '\n') {
-			*cur_line = '\0';
-			return (++cur_line);
-		}
-	}
-	
-	return NULL;
-}
-
-#define TOKENS_CHUNK (5)
-
-int tokenize_lines(	char *data, const char *delim,
-					int (*ptk)(const char**, size_t, size_t, void*), 
-					void* arg)
-{
-	assert(data != NULL);
-	assert(delim != NULL);
-	assert(ptk != NULL);
-	
-	int retval = 0;
-	
-	size_t tokens_size = TOKENS_CHUNK;
-	const char** tokens = reinterpret_cast<const char**>
-				(exiting_calloc(tokens_size, sizeof(char*)));
-				
-	char *cur_line = data;
-	for (size_t nline = 0; cur_line != NULL; ++nline) {
-		char *new_line = next_line(cur_line);
-		
-		size_t ntokens = 0;
-		for (	char *token = strtok(cur_line, delim);
-				token != NULL;
-				token = strtok(NULL, delim)) {
-			
-			if (ntokens == tokens_size) {
-				tokens_size += TOKENS_CHUNK;
-				tokens = reinterpret_cast<const char**>(
-					exiting_realloc(tokens, tokens_size * sizeof(char*))
-				);
-			}
-			
-			tokens[ntokens++] = token;
-		}
-		
-		retval = ptk(tokens, ntokens, nline, arg);
-		if (retval != 0)
-			break;
-		
-		cur_line = new_line;
-	}
-	
-	free(tokens);
-	
-	return retval;
-}
-
-const char *commands[] = {
-	"push",
-	"pop",
-	"add",
-	"mul",
-	"div"
-};
-
-int get_command_id(const char *name)
-{
-	for (int i = 0; i < sizeof(commands) / sizeof(*commands); ++i) {
-		if (strcmp(commands[i], name) == 0)
-			return i;
-	}
-	
-	return -1;
-}
-
-int process_push(const char* args[], size_t argc)
-{
-	printf("push\n");
-	return 0;
-}
-
-int process_add(const char* args[], size_t argc)
-{
-	printf("add\n");
-	return 0;
-}
-
-int process_mul(const char* args[], size_t argc)
-{
-	printf("mul\n");
-	return 0;
-}
-
-int process_div(const char* args[], size_t argc)
-{
-	printf("div\n");
-	return 0;
-}
-
-int process_pop(const char* args[], size_t argc)
-{
-	printf("pop\n");
-	return 0;
-}
-
-int (*processors[]) (const char**, size_t) = {
-	process_push,
-	process_pop,
-	process_add,
-	process_mul,
-	process_div
-};
-
 int process_tokens(	const char **tokens, size_t ntokens, 
-					size_t nline, void *arg)
+					size_t nline, void* arg)
 {
 	if (ntokens == 0)
 		return 0;
@@ -225,7 +81,7 @@ int process_tokens(	const char **tokens, size_t ntokens,
 	int id = get_command_id(tokens[0]);
 	
 	if (id < 0) {
-		printf("## ERROR: Unknown command %s on line %lu\n", 
+		printf("## ERROR: Unknown command \"%s\" on line %lu\n", 
 			tokens[0], nline);
 		
 		return -1;
@@ -239,9 +95,11 @@ int process_tokens(	const char **tokens, size_t ntokens,
 	printf("\n");
 	*/
 	
-	int res = processors[id](tokens, ntokens);
 	
-	return 0;
+	CommandsContainer* container = reinterpret_cast<CommandsContainer*>(
+		arg
+	);
+	return processors[id](tokens, ntokens, container);
 }
 
 int main()
@@ -250,7 +108,16 @@ int main()
 	
 	char *file = read_file(fname);
 	
-	tokenize_lines(	file, " \t;,.", process_tokens, NULL);
+	CommandsContainer* container = CommandsContainerInit();
+	
+	tokenize_lines(	file, " \t;,.", process_tokens, container);
+	
+	CommandsContainerShrink(container);
+	
+	for (int i = 0; i < container->file->ncommands; ++i)
+		printf("%d %d %d\n", 	container->file->commands[i].type,
+								container->file->commands[i].arg1, 
+								container->file->commands[i].arg2);
 	
 	printf("Done\n");
 }
